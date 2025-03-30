e61002c48bf339abb5efa15dc8590f704d2ff24a
danny <dannydospuntosv@gmail.com>
2025-03-30 11:46:57 -0500

    add fonts, configure fonts, configure icons, etc

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "url";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import Components from "unplugin-vue-components/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        tailwindcss()
+        tailwindcss(),
+        Components({
+            resolvers: [ IconsResolver() ]
+        }),
+        Icons({ compiler: "vue3", autoInstall: true })
     ],
     server: {
         host: true,
