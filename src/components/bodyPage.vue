b8f779296d3a7c22fef5dc6a5a17857db2c95fef
danny <dannydospuntosv@gmail.com>
2025-04-06 02:04:33 -0500

    add information page, update and fixed style content and rename page.

--- src/components/bodyPage.vue
+++ src/components/bodyPage.vue
@@ -3,7 +3,9 @@
 import ContentPage from "@/components/contentPage.vue";
 import portTarapoto from "@/assets/images/portTarapoto.jpg";
 import happyYoung from "@/assets/images/happyYoung.jpg";
-
+import LocationEvent from "@/components/locationEvent.vue";
+import TeamContent from "@/components/teamContent.vue";
+import FooterPage from "@/components/footerPage.vue";
 
 </script>
 
@@ -38,14 +40,7 @@ import happyYoung from "@/assets/images/happyYoung.jpg";
         </div>
 
         <content-page gradient-mode="to bottom right" color-via="var(--color-primary-100)" color-from="var(--color-primary-500)"
-                      color-to="var(--color-primary-700)" :opacity="0.65" :url-image="happyYoung">
-            <template #description>
-                no c k poner aki xd
-            </template>
-            <template #bottom-description>
-                tetas
-            </template>
-        </content-page>
+                      color-to="var(--color-primary-700)" :opacity="0.65" :url-image="happyYoung"/>
 
         <div class="grid w-full justify-items-center px-4 py-10 bg-slate-950">
             <p class="text-center text-3xl font-info md:text-5xl">
@@ -59,16 +54,13 @@ import happyYoung from "@/assets/images/happyYoung.jpg";
                 Vive la experiencia de Kadosh. Pon al servicio de la comunidad los dones y talentos que Dios te ha dado, mientras que
                 impactamos.
             </p>
-            <!-- Responsive container: switches from column on mobile to row on md+ -->
             <div class="mt-10 flex w-full flex-col items-center gap-6 md:flex-row">
-                <!-- Video container with aspect ratio (16:9) -->
                 <div class="w-full md:w-1/2">
-                    <div class="relative w-full bg-indigo-500 pb-80">
-                        <iframe class="absolute top-0 left-0 h-full w-full" title="YouTube video player" allowfullscreen
-                                src="https://www.youtube.com/embed/AbQtqh_eUCM?si=tUnfe-P4WnjFchEM"/>
+                    <div class="relative w-full pb-80">
+                        <iframe class="absolute top-0 left-0 h-full w-full rounded-2xl" title="YouTube video player" allowfullscreen
+                                src="https://www.youtube.com/embed/Bn4Qi1Wuxb0?si=9nzLjBR2UXK1aR4T"/>
                     </div>
                 </div>
-                <!-- Text container -->
                 <div class="w-full px-4 text-center text-2xl font-info md:w-1/2 md:text-4xl">
                     Toda la ciudad sabrá que estos que han trastornado al mundo...
                     <span class="block font-bold">TAMBIÉN HAN LLEGADO A TARAPOTO</span>
@@ -76,30 +68,14 @@ import happyYoung from "@/assets/images/happyYoung.jpg";
             </div>
         </div>
 
-        <content-page gradient-mode="to bottom right" color-via="var(--color-primary-100)" color-from="var(--color-primary-500)"
-                      color-to="var(--color-primary-700)" :opacity="0.65"
-                      url-image="https://as2.ftcdn.net/v2/jpg/02/56/10/07/1000_F_256100731_qNLp6MQ3FjYtA3Freu9epjhsAj2cwU9c.jpg">
-            <template #description>
-                no c k poner aki xd
-            </template>
-        </content-page>
+        <location-event/>
 
-        <div class="grid w-full justify-items-center px-4 py-5 bg-slate-950">
-            <div class="flex w-full flex-col items-center gap-6 md:flex-row">
-                <!-- Video container with aspect ratio (16:9) -->
-                <div class="w-full md:w-1/2">
-                    <div class="relative w-full pb-80">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7969.033943686046!2d-79.08876090408359!3d-8.088177683828809!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91ad3c3fb3b141e3%3A0xabcdff834d4deeaa!2sColegio%20Militar%20Gran%20Maricon%20Ram%C3%B3n%20Castilla!5e0!3m2!1sen!2sco!4v1717595544404!5m2!1sen!2sco"
-                                loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-                                class="absolute top-0 left-0 rounded-2xl h-full w-full sectionVenue__Map-sc-15l7hda-2 gzfAHm"/>
-                    </div>
-                </div>
-                <!-- Text container -->
-                <div class="w-full px-4 text-center text-2xl font-info md:w-1/2 md:text-4xl">
-                    ¿Dónde?
-                    <span class="block font-bold">TAMBIÉN HAN LLEGADO A TARAPOTO</span>
-                </div>
+        <div class="flex w-full items-center justify-center py-10 text-center text-3xl font-info bg-slate-950 md:text-5xl">
+            <div>
+                "Kadosh es el desafío a aceptar el llamado de Dios a tu vida."
             </div>
         </div>
+        <team-content/>
+        <footer-page/>
     </main>
 </template>
--- src/components/headerMenu.vue
+++ src/components/headerMenu.vue
@@ -22,10 +22,10 @@ const toggleMobileMenu = () => {
 };
 
 const menuItems = [
-    { label: "¿De qué se trata?" },
-    { label: "Agenda" },
-    { label: "Speakers" },
-    { label: "Labs" }
+    { currentPage: "#about", label: "¿De qué se trata?" },
+    // { currentPage: "", label: "Agenda" },
+    { currentPage: "", label: "Nuestro equipo" },
+    { currentPage: "", label: "Ubicanos" }
 ];
 
 const specialItem = { label: "Inscríbete", special: true };
@@ -37,7 +37,7 @@ const specialItem = { label: "Inscríbete", special: true };
         <nav class="container mx-auto flex items-center justify-between">
             <!-- Logo -->
             <div aria-description="header logo business" class="flex justify-center">
-                <img src="../assets/images/kadosh.png" alt="business logo" class="h-16 w-12"/>
+                <img src="../assets/images/kadosh.png" alt="business logo" class="h-16"/>
             </div>
             <!-- Botón de menú móvil (visible solo en móviles) -->
             <button class="block p-2 md:hidden" @click="toggleMobileMenu" aria-label="Toggle menu">
@@ -60,7 +60,7 @@ const specialItem = { label: "Inscríbete", special: true };
         <!-- Menú móvil desplegable con transición (visible solo en móviles) -->
         <transition name="slide-fade">
             <div v-if="isMobileMenuOpen" class="mt-2 md:hidden max-h-[50vh] overflow-y-auto">
-                <ul class="flex flex-col p-4 space-y-2" :class="isScrolled ? 'bg-slate-950/70' : 'bg-red-950/80'">
+                <ul class="flex flex-col p-4 space-y-2" :class="isScrolled ? 'bg-slate-950/70' : 'bg-slate-950/80'">
                     <li v-for="(item, index) in menuItems" :key="index" class="items-header">
                         <a aria-description="items-header">{{ item.label }}</a>
                     </li>
